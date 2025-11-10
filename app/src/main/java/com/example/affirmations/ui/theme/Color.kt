969ddda8d037ce383b9aff4ac87c6120a9393066
lrbraxton2-ui <lrbraxton2@gmail.com>
2025-11-09 21:29:20 -0500

    Scroll List app
    # Conflicts:
    #	.gitignore
    #	app/.gitignore
    #	app/build.gradle.kts
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/drawable/ic_launcher_background.xml
    #	app/src/main/res/drawable/ic_launcher_foreground.xml
    #	app/src/main/res/mipmap-anydpi-v26/ic_launcher.xml
    #	app/src/main/res/mipmap-anydpi-v26/ic_launcher_round.xml
    #	app/src/main/res/values/strings.xml
    #	app/src/main/res/values/themes.xml
    #	build.gradle.kts
    #	gradle.properties
    #	gradle/wrapper/gradle-wrapper.jar
    #	gradle/wrapper/gradle-wrapper.properties
    #	gradlew
    #	gradlew.bat
    #	settings.gradle.kts

--- app/src/main/java/com/example/affirmations/ui/theme/Color.kt
+++ app/src/main/java/com/example/affirmations/ui/theme/Color.kt
@@ -13,10 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    id("com.android.application") version "8.13.0" apply false
-    id("com.android.library") version "8.13.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
-}
+package com.example.affirmations.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val md_theme_light_background = Color(0xFFFBFCFE)
+val md_theme_light_surfaceVariant = Color(0xFFE7E0EC)
+val md_theme_light_onSurfaceVariant = Color(0xFF49454f)
+
+val md_theme_dark_background = Color(0xFF191C1D)
+val md_theme_dark_surfaceVariant = Color(0xFF49454f)
+val md_theme_dark_onSurfaceVariant = Color(0xFFCAC4D0)
